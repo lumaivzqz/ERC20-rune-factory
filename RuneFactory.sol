da37eecd773527f1e8999e2d477d8db0761213d5
Lucia Vázquez <42591425+lumaivzqz@users.noreply.github.com>
2024-05-24 18:41:45 +0200

    Update RuneFactory.sol

--- RuneFactory.sol
+++ RuneFactory.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Rune is ERC20, Ownable {
+    constructor(string memory name, string memory symbol, uint256 initialSupply, address initialOwner) 
+        ERC20(name, symbol)
+        Ownable(initialOwner) {
+        _mint(msg.sender, initialSupply);
+    }
+}
+
+contract RuneFactory {
+    event TokenCreated(address tokenAddress);
+    
+    function createRune(string memory name, string memory symbol, uint256 initialSupply, address initialOwner) public {
+        MyERC20Token newToken = new MyERC20Token(name, symbol, initialSupply, initialOwner);
+        emit TokenCreated(address(newToken));
+    }
+}
