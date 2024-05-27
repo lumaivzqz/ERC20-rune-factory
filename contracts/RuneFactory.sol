cc9f59030598c700e1e1c9d155b9674d93c99d0b
Sebastian Guaqueta <scguaquetam@gmail.com>
2024-05-27 16:21:40 -0500

    feat: adding additional info to rune factory

--- contracts/RuneFactory.sol
+++ contracts/RuneFactory.sol
@@ -6,24 +6,37 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Rune is ERC20, Ownable {
-    constructor(string memory name, string memory symbol, uint256 initialSupply, address initialOwner) 
+    uint256 private mintAmount;
+    uint256 private maxSupply;
+    constructor(string memory name, string memory symbol, uint256 initialSupply, address initialOwner, uint256 _mintAmount, uint256 _maxSupply)
         ERC20(name, symbol)
         Ownable(initialOwner) {
         _mint(initialOwner, initialSupply);
+        mintAmount = _mintAmount;
+        maxSupply = _maxSupply;
     }
-    function mint(address to, uint256 amount) public {
-        _mint(to, amount);
+    function mint(address to) public {
+        require(totalSupply() + mintAmount <= maxSupply, "Mint amount exceeds max supply");
+        _mint(to, mintAmount);
+    }
+    function getMintAmount() public view returns (uint256) {
+        return mintAmount;
+    }
+    function getMaxSupply() public view returns (uint256) {
+        return maxSupply;
     }
 }
 
 contract RuneFactory {
-    event TokenCreated(address tokenAddress);
+    event TokenCreated(address indexed tokenAddress, string indexed name, string indexed symbol, uint256 initialSupply, address initialOwner, uint256 runeIDBTC);
     struct Token {
         string name;
         string symbol;
         uint256 initialSupply;
         address initialOwner;
         uint256 runeIDBTC;
+        uint256 mintAmount;
+        uint256 maxSupply;
     }
     Token[] public tokens;
 
@@ -39,10 +52,18 @@ contract RuneFactory {
         return tokens;
     }
 
-    function getTokenAddress(string memory name, string memory symbol, uint256 initialSupply, address initialOwner, bytes32 salt) public view returns (address) {
+    function getTokenAddress(
+        string memory name, 
+        string memory symbol, 
+        uint256 initialSupply, 
+        address initialOwner, 
+        bytes32 salt, 
+        uint256 _mintAmount,
+        uint256 _maxSupply
+    ) public view returns (address) {
         bytes memory bytecode = abi.encodePacked(
             type(Rune).creationCode,
-            abi.encode(name, symbol, initialSupply, initialOwner)
+            abi.encode(name, symbol, initialSupply, initialOwner, _mintAmount, _maxSupply)
         );
         bytes32 hash = keccak256(
             abi.encodePacked(
@@ -55,11 +76,20 @@ contract RuneFactory {
         return address(uint160(uint(hash)));
     }
 
-    function createRune(string memory name, string memory symbol, uint256 initialSupply, address initialOwner, uint256 runeIDBTC, bytes32 salt) public {
-        address predictedAddress = getTokenAddress(name, symbol, initialSupply, initialOwner, salt);
-        Rune newToken = new Rune{salt: salt}(name, symbol, initialSupply, initialOwner);
+    function createRune(
+        string memory name, 
+        string memory symbol, 
+        uint256 initialSupply,
+        address initialOwner, 
+        uint256 runeIDBTC, 
+        bytes32 salt, 
+        uint256 _mintAmount, 
+        uint256 _maxSupply
+    ) public {
+        address predictedAddress = getTokenAddress(name, symbol, initialSupply, initialOwner, salt, _mintAmount, _maxSupply);
+        Rune newToken = new Rune{salt: salt}(name, symbol, initialSupply, initialOwner, _mintAmount, _maxSupply);
         require(address(newToken) == predictedAddress, "Address prediction failed");
-        tokens.push(Token(name, symbol, initialSupply, initialOwner, runeIDBTC));
-        emit TokenCreated(address(newToken));
+        tokens.push(Token(name, symbol, initialSupply, initialOwner, runeIDBTC, _mintAmount, _maxSupply));
+        emit TokenCreated(address(newToken), name, symbol, initialSupply, initialOwner, runeIDBTC);
     }
 }
--- contracts/RuneNFT.sol
+++ contracts/RuneNFT.sol
@@ -6,12 +6,23 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Rune is ERC20, Ownable {
-    constructor(string memory name, string memory symbol, uint256 initialSupply, address initialOwner) 
+    uint256 private mintAmount;
+    uint256 private maxSupply;
+    constructor(string memory name, string memory symbol, uint256 initialSupply, address initialOwner, uint256 _mintAmount, uint256 _maxSupply)
         ERC20(name, symbol)
         Ownable(initialOwner) {
         _mint(initialOwner, initialSupply);
+        mintAmount = _mintAmount;
+        maxSupply = _maxSupply;
     }
-    function mint(address to, uint256 amount) public {
-        _mint(to, amount);
+    function mint(address to) public {
+        require(totalSupply() + mintAmount <= maxSupply, "Mint amount exceeds max supply");
+        _mint(to, mintAmount);
+    }
+    function getMintAmount() public view returns (uint256) {
+        return mintAmount;
+    }
+    function getMaxSupply() public view returns (uint256) {
+        return maxSupply;
     }
 }
